6dbf53d0a1959f36c954c1b1b3f4fe6cf856ab9b
itochs <halglobe0108@gmail.com>
2022-12-17 13:36:31 +0900

    check hot to use svg rect

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const width = 600;
+  const height = 600;
+
   return (
     <div>
       <Head>
@@ -14,6 +17,11 @@ export default function Home() {
         <header>this is header</header>
         <h1>main</h1>
         <p>良い感じのコンテンツ</p>
+        <div id="graph">
+          <svg id="content" viewBox={`0 0 ${width} ${height}`}>
+            <rect x={100} y={100} width={100} height={100}></rect>
+          </svg>
+        </div>
       </main>
 
       <footer>
